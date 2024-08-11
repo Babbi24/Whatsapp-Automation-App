3b1aba8ab167b6b5b7430926b44e073d7095e794
Babbi24 <babbi113@gmail.com>
2024-08-11 19:00:21 +0530

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,19 @@
+import pywhatkit
+import streamlit as st
+
+
+from datetime import datetime
+phonelist = st.text_input("Please input numbers you want to automate with seperated by ,")
+message = st.text_area("Enter your Message")
+submit = st.button("Enter")
+
+phone_number = "+919550787234"
+message = "Hello, this is an automated message!"
+hour = 18 # 11 PM
+minute = 20
+try:
+    pywhatkit.sendwhatmsg(phone_number, message, hour, minute)
+    print("Message scheduled successfully!")
+except Exception as e:
+       print(f"An error occurred: {e}")
+st.success("Message Sent!")
